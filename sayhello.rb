7d1f093008333c2e939beb56849e8d3e18036f46
ed-chin <ed-chin@users.noreply.github.com>
2016-09-08 09:33:33 -0700

    simple formula added

---
+++ sayhello.rb
@@ -0,0 +1,13 @@
+class Sayhello < Formula
+  desc "Simple brew formula"
+  homepage "https://github.com/ed-chin/sayhello/"
+  url "https://github.com/ed-chin/sayhello/archive/1.0.0.tar.gz"
+  version "1.0.0"
+  sha256 "bdb86cd7153aa593dc0c868d7c4e7ba7bc67d21a30f8c03bd8f1b8e9cf72bfab"
+
+  def install
+  end
+
+  test do
+  end
+end
